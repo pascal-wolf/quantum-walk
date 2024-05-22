0275296c5ca3870a8060be78e9a0482c4f9d0504
pascal-wolf <pwolf96@icloud.com>
2024-05-22 18:11:22 +0200

    refactor: :art: improve structure and add doc strings

--- app.py
+++ app.py
@@ -1,123 +1,8 @@
-import pandas as pd
-from dash import Dash, Input, Output, dcc, html
-
+from dash import Input, Output
+from layout import create_app
 from walk import perform_quantum_walk, perform_random_walk
 
-external_stylesheets = [
-    {
-        "href": (
-            "https://fonts.googleapis.com/css2?" "family=Lato:wght@400;700&display=swap"
-        ),
-        "rel": "stylesheet",
-    },
-]
-app = Dash(__name__, external_stylesheets=external_stylesheets)
-app.title = "Discret Quantum Walk"
-
-app.layout = html.Div(
-    children=[
-        html.Div(
-            children=[
-                html.H3(children="Quantum Walk", className="header-title"),
-                html.P(
-                    children=("Simulation of a Discret Quantum walk"),
-                    className="header-description",
-                ),
-            ],
-            className="header",
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=[
-                        html.Div(children="Type", className="menu-title"),
-                        dcc.Dropdown(
-                            ["Random", "Quantum"],
-                            "Quantum",
-                            id="type-filter",
-                            clearable=False,
-                            searchable=False,
-                            className="dropdown",
-                        ),
-                    ]
-                ),
-                html.Div(
-                    children=[
-                        html.Div(children="Coin", className="menu-title"),
-                        dcc.Dropdown(
-                            ["0", "1", "Symmetric"],
-                            "1",
-                            id="coin-filter",
-                            clearable=False,
-                            searchable=False,
-                            className="dropdown",
-                        ),
-                    ]
-                ),
-            ],
-            className="menu",
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=[
-                        html.Div(children="Number of Qubits", className="menu-title-b"),
-                        dcc.Slider(
-                            3,
-                            10,
-                            1,
-                            value=7,
-                            id="n_qubits-filter",
-                            marks=None,
-                            tooltip={"placement": "bottom", "always_visible": True},
-                        ),
-                    ]
-                ),
-                html.Div(
-                    children=[
-                        html.Div(children="Number of Steps", className="menu-title-b"),
-                        dcc.Slider(
-                            1,
-                            100,
-                            1,
-                            value=30,
-                            id="n_steps-filter",
-                            marks=None,
-                            tooltip={"placement": "bottom", "always_visible": True},
-                        ),
-                    ]
-                ),
-                html.Div(
-                    children=[
-                        html.Div(children="Repetition", className="menu-title-b"),
-                        dcc.Slider(
-                            5000,
-                            10000,
-                            500,
-                            value=5000,
-                            id="repetitions-filter",
-                            marks=None,
-                            tooltip={"placement": "bottom", "always_visible": True},
-                        ),
-                    ]
-                ),
-            ],
-            className="menu-b",
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=dcc.Graph(
-                        id="price-chart",
-                        config={"displayModeBar": False},
-                    ),
-                    className="card",
-                ),
-            ],
-            className="wrapper",
-        ),
-    ]
-)
+app = create_app()
 
 
 @app.callback(
@@ -129,6 +14,9 @@ app.layout = html.Div(
     Input("coin-filter", "value"),
 )
 def update_charts(n_qubits, n_steps, repetitions, walk_type, coin):
+    """
+    Update the charts based on the selected parameters.
+    """
     if walk_type.lower() == "quantum":
         symmetric = False
         if coin == "0":
@@ -153,7 +41,16 @@ def update_charts(n_qubits, n_steps, repetitions, walk_type, coin):
         walk_type = ""
         print("No walk type selected. Setting arrays to zero!")
 
-    price_chart_figure = {
+    price_chart_figure = create_price_chart_figure(x_array, y_array, walk_type)
+
+    return price_chart_figure
+
+
+def create_price_chart_figure(x_array, y_array, walk_type):
+    """
+    Create the figure for the price chart.
+    """
+    return {
         "data": [
             {
                 "x": x_array,
@@ -173,8 +70,6 @@ def update_charts(n_qubits, n_steps, repetitions, walk_type, coin):
             "colorway": ["#17B897"],
         },
     }
-
-    return price_chart_figure
 
 
 if __name__ == "__main__":
---
+++ layout.py
@@ -0,0 +1,133 @@
+from dash import Dash, dcc, html
+
+
+def create_app():
+    """
+    Create and configure the Dash application.
+    """
+    external_stylesheets = [
+        {
+            "href": (
+                "https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
+            ),
+            "rel": "stylesheet",
+        },
+    ]
+    app = Dash(__name__, external_stylesheets=external_stylesheets)
+    app.title = "Discret Quantum Walk"
+    app.layout = create_layout()
+    return app
+
+
+def create_layout():
+    """
+    Create the layout for the Dash application.
+    """
+    return html.Div(
+        children=[
+            create_header(),
+            create_menu(),
+            create_menu_b(),
+            create_wrapper(),
+        ]
+    )
+
+
+def create_header():
+    """
+    Create the header for the Dash application.
+    """
+    return html.Div(
+        children=[
+            html.H3(children="Quantum Walk", className="header-title"),
+            html.P(
+                children=("Simulation of a Discret Quantum walk"),
+                className="header-description",
+            ),
+        ],
+        className="header",
+    )
+
+
+def create_menu():
+    """
+    Create the menu for the Dash application.
+    """
+    return html.Div(
+        children=[
+            create_dropdown("Type", ["Random", "Quantum"], "Quantum", "type-filter"),
+            create_dropdown("Coin", ["0", "1", "Symmetric"], "1", "coin-filter"),
+        ],
+        className="menu",
+    )
+
+
+def create_menu_b():
+    """
+    Create the secondary menu for the Dash application.
+    """
+    return html.Div(
+        children=[
+            create_slider("Number of Qubits", 3, 10, 1, 7, "n_qubits-filter"),
+            create_slider("Number of Steps", 1, 100, 1, 30, "n_steps-filter"),
+            create_slider("Repetition", 5000, 10000, 500, 5000, "repetitions-filter"),
+        ],
+        className="menu-b",
+    )
+
+
+def create_wrapper():
+    """
+    Create the wrapper for the Dash application.
+    """
+    return html.Div(
+        children=[
+            html.Div(
+                children=dcc.Graph(
+                    id="price-chart",
+                    config={"displayModeBar": False},
+                ),
+                className="card",
+            ),
+        ],
+        className="wrapper",
+    )
+
+
+def create_dropdown(title, options, default, id):
+    """
+    Create a dropdown menu with the given parameters.
+    """
+    return html.Div(
+        children=[
+            html.Div(children=title, className="menu-title"),
+            dcc.Dropdown(
+                options,
+                default,
+                id=id,
+                clearable=False,
+                searchable=False,
+                className="dropdown",
+            ),
+        ]
+    )
+
+
+def create_slider(title, min, max, step, default, id):
+    """
+    Create a slider with the given parameters.
+    """
+    return html.Div(
+        children=[
+            html.Div(children=title, className="menu-title-b"),
+            dcc.Slider(
+                min,
+                max,
+                step,
+                value=default,
+                id=id,
+                marks=None,
+                tooltip={"placement": "bottom", "always_visible": True},
+            ),
+        ]
+    )
