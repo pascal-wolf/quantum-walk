f362273bed2246d6e06b6c4328f26d311d873c62
pascal-wolf <pwolf96@icloud.com>
2024-05-22 23:29:38 +0200

    refactor: :recycle: remove not needed code

--- utils.py
+++ utils.py
@@ -1,25 +1,7 @@
-"""
-Maybe needed in the future for dynamic inital value selection:
-"""
-
-
-def bit_to_number(n_qubits):
-    bit_string = ""
-    for i in range(n_qubits):
-        bit_string += "1"
-    return int(bit_string, 2)
-
-
-def number_to_bit(number):
-    return f"{number:b}"
-
-
-"""
-Disclaimer: This function is taken by the Google Tutorial about Random Walks
-"""
-
-
 def get_values_from_dict(final):
+    """
+    Disclaimer: This function is taken by the Google Tutorial about Random Walks
+    """
 
     x_arr = list(final.keys())
     y_arr = [dict(final)[j] for j in dict(final).keys()]
