42af3ee4c526c7ff5fd5dc6ae73c0f8f36f8ed13
pascal-wolf <pwolf96@icloud.com>
2024-05-22 19:48:53 +0200

    docs: :memo: add docstring

--- walk.py
+++ walk.py
@@ -12,7 +12,24 @@ def perform_quantum_walk(
     verbose=False,
     percentage=True,
 ):
+    """
+    Perform a quantum walk.
 
+    This function simulates a quantum walk for a given number of repetitions. It returns the x and y arrays
+    for the quantum walk. If the percentage flag is set to True, it returns the percentage of each outcome.
+
+    Args:
+        number_qubits (int): The number of qubits.
+        n_steps (int): The number of steps.
+        repetitions (int): The number of repetitions.
+        coin_set (bool, optional): If True, the coin is set. Defaults to True.
+        symmetric (bool, optional): If True, the walk is symmetric. Defaults to False.
+        verbose (bool, optional): If True, print the circuit. Defaults to False.
+        percentage (bool, optional): If True, return the percentage of each outcome. Defaults to True.
+
+    Returns:
+        tuple: The x and y arrays for the quantum walk.
+    """
     qubits = cirq.GridQubit.rect(1, number_qubits)
     circuit = cirq.Circuit()
 
@@ -39,6 +56,19 @@ def perform_quantum_walk(
 
 
 def run_simulation(circuit, repetitions):
+    """
+    Run a simulation.
+
+    This function simulates the given circuit for a specified number of repetitions. It returns a dictionary
+    with the histogram of the simulation results.
+
+    Args:
+        circuit (cirq.Circuit): The circuit to simulate.
+        repetitions (int): The number of repetitions.
+
+    Returns:
+        dict: The histogram of the simulation results.
+    """
     simulator = cirq.Simulator()
     result = simulator.run(circuit, repetitions=repetitions)
     final = result.histogram(key="x")
@@ -46,7 +76,19 @@ def run_simulation(circuit, repetitions):
 
 
 def one_quantum_step(circuit, qubits):
+    """
+    Perform one step of a quantum walk.
 
+    This function applies a Hadamard operator, left shift operator, and right shift operator to the given circuit
+    and qubits. It returns the updated circuit.
+
+    Args:
+        circuit (cirq.Circuit): The circuit to perform the step on.
+        qubits (list): The qubits to perform the step with.
+
+    Returns:
+        cirq.Circuit: The circuit after performing the step.
+    """
     number_qubits = len(qubits)
     qubits = cirq.GridQubit.rect(1, number_qubits)
     # Hadarmard Operator
@@ -57,8 +99,8 @@ def one_quantum_step(circuit, qubits):
         controlling_qubits = [
             cirq.GridQubit(0, v) for v in range(number_qubits - 1, i - 1, -1)
         ]
-        cnX = cirq.X.on(cirq.GridQubit(0, i - 1)).controlled_by(*controlling_qubits)
-        circuit.append(cnX)
+        cn_x = cirq.X.on(cirq.GridQubit(0, i - 1)).controlled_by(*controlling_qubits)
+        circuit.append(cn_x)
 
     # Right shift Operator
     for i in range(number_qubits - 1, 0, -1):
@@ -67,8 +109,8 @@ def one_quantum_step(circuit, qubits):
         controlling_qubits = [
             cirq.GridQubit(0, v) for v in range(number_qubits - 1, i - 1, -1)
         ]
-        cnX = cirq.X.on(cirq.GridQubit(0, i - 1)).controlled_by(*controlling_qubits)
-        circuit.append(cnX)
+        cn_x = cirq.X.on(cirq.GridQubit(0, i - 1)).controlled_by(*controlling_qubits)
+        circuit.append(cn_x)
 
     for i in range(1, number_qubits):
         circuit.append(cirq.X.on(cirq.GridQubit(0, i)))
@@ -76,16 +118,24 @@ def one_quantum_step(circuit, qubits):
     return circuit
 
 
-"""
-Disclaimer: This function is taken by the Google Tutorial about Random Walks
-"""
-
-
 def one_random_step(pr, n_steps, position):
+    """
+    Disclaimer: This function is taken by the Google Tutorial about Random Walks
 
+    Perform one step of a random walk.
+
+    This function repeatedly queries a random variable and moves the walker for the specified number of steps.
+
+    Args:
+        pr (float): The probability of moving right.
+        n_steps (int): The number of steps.
+        position (int): The initial position.
+
+    Returns:
+        int: The position after performing the step.
+    """
     # Repeatedly queries our random variable and moves our walker for the specified number of steps
-
-    for j in range(n_steps):
+    for _ in range(n_steps):
 
         coin_flip = list(
             np.random.choice(2, 1, p=[1 - pr, pr])
@@ -98,9 +148,24 @@ def one_random_step(pr, n_steps, position):
 def perform_random_walk(
     repetitions, n_steps, pr=0.5, initial_position=0, percentage=True
 ):
-    # positions = np.arange(-1 * n_steps, n_steps + 1, 1)
+    """
+    Perform a random walk.
+
+    This function simulates a random walk for a given number of repetitions. It returns the positions and instances
+    of the walk. If the percentage flag is set to True, it returns the percentage of each outcome.
+
+    Args:
+        repetitions (int): The number of repetitions.
+        n_steps (int): The number of steps.
+        pr (float, optional): The probability of moving right. Defaults to 0.5.
+        initial_position (int, optional): The initial position. Defaults to 0.
+        percentage (bool, optional): If True, return the percentage of each outcome. Defaults to True.
+
+    Returns:
+        tuple: The positions and instances for the random walk.
+    """
     positions = range(-1 * n_steps, n_steps + 1)
-    instances = [0 for i in range(-1 * n_steps, n_steps + 1)]
+    instances = [0 for _ in range(-1 * n_steps, n_steps + 1)]
 
     for _ in range(repetitions):
 
